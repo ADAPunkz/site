5c3323f9aba1211f12e228961c76c472b69547d2
transiti0nary <cloudline@protonmail.com>
2022-01-19 19:12:49 +0000

    npm update, page boilerplate

---
+++ src/pages/mvp.tsx
@@ -0,0 +1,13 @@
+import { Box } from 'grommet';
+
+import Layout from '../components/Layout';
+
+const MVP = () => {
+  return (
+    <Layout>
+      <Box></Box>
+    </Layout>
+  );
+};
+
+export default MVP;
---
+++ src/pages/pool.tsx
@@ -0,0 +1,13 @@
+import { Box } from 'grommet';
+
+import Layout from '../components/Layout';
+
+const Pool = () => {
+  return (
+    <Layout>
+      <Box></Box>
+    </Layout>
+  );
+};
+
+export default Pool;
