ccedd65e2f0ee9a5d7d89c10fc73d6f0f4957e4d
transiti0nary <cloudline@protonmail.com>
2022-01-28 23:45:49 +0000

    alter naming

--- src/components/CollageAttributes.tsx
+++ src/components/CollageAttributes.tsx
@@ -8,7 +8,7 @@ import NftAttribute from './NftAttribute';
 const CollageAttributes: FC<{ metadata: CollageNft }> = ({ metadata }) => (
   <Box direction="column" gap="small">
     <NftAttribute name="Edition" icon={<Tag color="terminal" />} value={metadata.name} />
-    <NftAttribute name="Rank" icon={<Scorecard color="terminal" />} value={metadata.rank.toString()} />
+    <NftAttribute name="Rank" icon={<Scorecard color="terminal" />} value={`Rank ${metadata.rank.toString()}`} />
     <NftAttribute name="Type" icon={<Accessibility color="terminal" />} value={`${metadata.type.value} (${metadata.type.percent}%)`} />
     <NftAttribute name="Tier" icon={<Lock color="terminal" />} value={`${metadata.tier.value} (${metadata.tier.percent}%)`} />
   </Box>
